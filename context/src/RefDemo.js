632174743a0665d7a3b6e9f3ece5e3c2a099d6c6
temuulen <temuulen.n@pinecone.mn>
2023-02-08 15:15:31 +0800

    added ref demo

---
+++ context/src/RefDemo.js
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from "react";
+
+const token = 1234;
+
+export default function RefDemo() {
+  const value = useRef("");
+
+  const inputParent = useRef(null);
+
+  useEffect(() => {
+    inputParent.current.childNodes[0].focus();
+  }, []);
+
+  const handleInput = (e) => {
+    value.current += e.target.value;
+    if (value.current.length < 4) {
+      inputParent.current.childNodes[value.current.length].focus();
+    } else {
+      inputParent.current.childNodes[3].value =
+        inputParent.current.childNodes[3].value[0] || "";
+    }
+  };
+
+  const handleFocus = (e) => {
+    const length = value.current.length;
+    if (length === 4) {
+      inputParent.current.childNodes[3].focus();
+    } else {
+      inputParent.current.childNodes[length].focus();
+    }
+  };
+
+  return (
+    <div className="otp-example" ref={inputParent}>
+      <input onChange={handleInput} onFocus={handleFocus} />
+      <input onChange={handleInput} onFocus={handleFocus} />
+      <input onChange={handleInput} onFocus={handleFocus} />
+      <input onChange={handleInput} onFocus={handleFocus} />
+    </div>
+  );
+}
